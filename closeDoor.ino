8995922a53b3564874d2efacf5872fa9efaf705f
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 23:01:57 +0200

    fixed the angles of the servo motor

--- closeDoor.ino
+++ closeDoor.ino
@@ -1,8 +1,8 @@
 void closeDoor() {
   printOnLCD("Closing Door...");
 
-  for (int angle = 90; angle >= 0; angle -= 5) {
-    doorServoMotor.write(angle);
-    delay(15);
+  for (int pos = 180; pos >= 90; pos -= 1) { // goes from 0 degrees to 180 degrees
+    doorServoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
   }
 }
