4b44a5d1550558ccecbdf07e234476019e9f7e79
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 19:03:38 +0200

    fixed some conflicts

--- closeDoor.ino
+++ closeDoor.ino
@@ -2,7 +2,7 @@ void closeDoor() {
   printOnLCD("Closing Door...");
 
   for (int angle = 90; angle >= 0; angle -= 5) {
-    servoMotor.write(angle);
+    doorServoMotor.write(angle);
     delay(15);
   }
 }
