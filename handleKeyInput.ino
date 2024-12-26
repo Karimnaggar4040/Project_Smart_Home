b377e0aec1074e975a89e9d55f77393794e39482
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 18:51:07 +0200

    optomization

--- handleKeyInput.ino
+++ handleKeyInput.ino
@@ -1,20 +1,20 @@
 void handleKeyInput(char key) {
   if (key == '#') {  // Clear input
     clearPassword();
-    printOnLCD("Cleared", "");
+    printOnLCD("Cleared");
   } else if (key == '*') {  // Submit password
     lcd.clear();
     if (strcmp(enteredPassword, MASTER_PASSWORD) == 0) {
-      printOnLCD("Access Granted", "");
+      printOnLCD("Access Granted");
       openDoor(3000);  // Keep door open for 3 seconds
     } else {
-      printOnLCD("Access Denied", "");
+      printOnLCD("Access Denied");
       delay(2000);
     }
     clearPassword();
   } else if (passwordIndex < PASSWORD_LENGTH - 1) {  // Add character to password
     enteredPassword[passwordIndex++] = key;
-    lcd.setCursor(passwordIndex - 1, 1);
+    // lcd.setCursor(passwordIndex - 1, 1);
     lcd.print('*');
   }
 }
