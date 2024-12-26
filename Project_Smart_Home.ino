578fbdd1f6bbda5675051c1a399a1135d2bdb591
Karimnaggar4040 <naggar@Naggar.>
2024-12-26 14:49:05 +0200

    added the fire alarm pins settings

--- Project_Smart_Home.ino
+++ Project_Smart_Home.ino
@@ -1,5 +1,19 @@
+// Initialize Pins
+const int buzzerPin = 12;
+const int flamePin = 11;
+
+// Initliaze states
+int Flame = HIGH;
+
 void setup() {
-    // Initialization code
+  // Output Pins
+  pinMode(buzzerPin, OUTPUT);
+
+  // Input Pins
+  pinMode(flamePin, INPUT);
+
+  // Begin serialization
+  Serial.begin(9600);
 }
 
 void loop() {
