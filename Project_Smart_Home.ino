8e58dc62bc7c6ba240e4f49987df6fc8a370a815
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 16:03:51 +0200

    added new pins

--- Project_Smart_Home.ino
+++ Project_Smart_Home.ino
@@ -1,8 +1,13 @@
 // Initialize Pins
-const int buzzerPin = 12;
+
+const int flameBuzzerPin = 12; //flame buzzer
 const int flamePin = 11;
-const int trigPin = 9;   //sends the wave
-const int echoPin = 10;  // receives the wave
+const int echoPin = 10;   // receives the wave
+const int trigPin = 9;    //sends the wave
+
+const int ldrPin = A0;    // LDR pins
+int ldrValue = 0;   // LDR pins
+const int ledPin = 13;
 
 // Initliaze states
 int Flame = HIGH;
@@ -11,8 +16,9 @@ float distance = 0;
 
 void setup() {
   // Output Pins
-  pinMode(buzzerPin, OUTPUT);
+  pinMode(flameBuzzerPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   // Input Pins
   pinMode(flamePin, INPUT);
   pinMode(echoPin, INPUT);
@@ -25,4 +31,5 @@ void loop() {
     // Main code
     flameSensor();
     ultraSonicSensor();
+    light_resistance();
 }
