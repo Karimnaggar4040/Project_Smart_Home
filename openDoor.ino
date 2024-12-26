d57a1a6b64721e20f226e1522bcf188278b9fa32
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 18:52:27 +0200

    fixed some conflicts

--- openDoor.ino
+++ openDoor.ino
@@ -1,5 +1,5 @@
 void openDoor(int delay_ms) {
-  printOnLCD("Opening Door...", "");
+  printOnLCD("Opening Door...");
 
   for (int angle = 0; angle <= 90; angle += 5) {
     servoMotor.write(angle);
