a8d79725708759b915c126f8b50a18dd53e890a1
Karimnaggar4040 <naggar@Naggar.>
2024-12-26 15:10:00 +0200

    Added the ultraSonicSenor file and called in the main

--- Project_Smart_Home.ino
+++ Project_Smart_Home.ino
@@ -1,16 +1,21 @@
 // Initialize Pins
 const int buzzerPin = 12;
 const int flamePin = 11;
+const int trigPin = 9;   //sends the wave
+const int echoPin = 10;  // receives the wave
 
 // Initliaze states
 int Flame = HIGH;
+long duration = 0;
+float distance = 0;
 
 void setup() {
   // Output Pins
   pinMode(buzzerPin, OUTPUT);
-
+  pinMode(trigPin, OUTPUT);
   // Input Pins
   pinMode(flamePin, INPUT);
+  pinMode(echoPin, INPUT);
 
   // Begin serialization
   Serial.begin(9600);
@@ -19,4 +24,5 @@ void setup() {
 void loop() {
     // Main code
     flameSensor();
+    ultraSonicSensor();
 }
