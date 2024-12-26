d8dec31e99adabff8e8b685d76adac4f5dc20902
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 18:05:38 +0200

    open and close door functions implementaion

---
+++ closeDoor.ino
@@ -0,0 +1,8 @@
+void closeDoor() {
+  printOnLCD("Closing Door...", "");
+
+  for (int angle = 90; angle >= 0; angle -= 5) {
+    servoMotor.write(angle);
+    delay(15);
+  }
+}
---
+++ openDoor.ino
@@ -0,0 +1,11 @@
+void openDoor(int delay_ms) {
+  printOnLCD("Opening Door...", "");
+
+  for (int angle = 0; angle <= 90; angle += 5) {
+    servoMotor.write(angle);
+    delay(15);
+  }
+
+  delay(delay_ms); // Keep the door open for a while
+  closeDoor();
+}
