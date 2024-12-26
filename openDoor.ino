8f9be1a44ff9f36975a2ab79da89cd5555d4ff68
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-26 23:02:02 +0200

    fixed the angles of the servo motor

--- openDoor.ino
+++ openDoor.ino
@@ -1,11 +1,11 @@
 void openDoor(int delay_ms) {
   printOnLCD("Opening Door...");
 
-  for (int angle = 0; angle <= 90; angle += 5) {
-    doorServoMotor.write(angle);
-    delay(15);
+  for (int pos = 90; pos <= 180; pos += 1) { // goes from 180 degrees to 0 degrees
+    doorServoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
   }
-
+  
   delay(delay_ms); // Keep the door open for a while
   closeDoor();
 }
