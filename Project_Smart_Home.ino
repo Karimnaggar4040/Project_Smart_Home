3ba6ae9bc20d44222c5f5dab000c523a41e6fdc0
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-27 19:26:52 +0200

    removed the unnecessary lines

--- Project_Smart_Home.ino
+++ Project_Smart_Home.ino
@@ -22,25 +22,15 @@ byte colPins[COLS] = { 3, 2, 9, 8 };
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 Servo doorServoMotor;
-Servo curtainsServoMotor;
-Servo mansharServoMotor;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  //sometimes the LCD adress is not 0x3f. Change to 0x27 if it dosn't work.
 
 // Initialize Pins
 const int flameBuzzerPin = 12;  //flame buzzer
 const int flamePin = 11;
-const int echoPin = 10;  // receives the wave
-const int trigPin = 9;   //sends the wave
-
-const int ledPin = 13;
-const int ldrPin = A0;  // LDR pins
 
 // Initliaze states
-int ldrValue = 0;  // LDR value
 int Flame = HIGH;
-long duration = 0;
-float distance = 0;
 
 char enteredPassword[PASSWORD_LENGTH] = { 0 };
 byte passwordIndex = 0;
@@ -48,15 +38,10 @@ byte passwordIndex = 0;
 void setup() {
   // Servo Motors attach
   doorServoMotor.attach(10);
-  // curtainsServoMotor.attach();
-  // mansharServoMotor.attach();
   // Output Pins
   pinMode(flameBuzzerPin, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
   // Input Pins
   pinMode(flamePin, INPUT);
-  pinMode(echoPin, INPUT);
 
   // Begin serialization
   Serial.begin(9600);
@@ -69,8 +54,6 @@ void setup() {
 void loop() {
   // Main code
   flameSensor();
-  ultraSonicSensor();
-  lightResistance();
 
   char customKey = customKeypad.getKey();
   if (customKey) {
