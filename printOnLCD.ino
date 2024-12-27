61163fb3eece8723c4393d1e39575587404e65e4
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-27 19:25:06 +0200

    change the delay value

--- printOnLCD.ino
+++ printOnLCD.ino
@@ -2,6 +2,6 @@ void printOnLCD(String text) {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(text);  // printing the recieved text from the website to the lcd
-  delay(1500);
+  delay(2000);
   lcd.clear();
 }
